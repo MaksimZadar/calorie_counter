e6e22a5fdd7dd55676c29aad7169067b530c9616
Max Zadarnovskiy <maxzadarnovskiyy@gmail.com>
2022-04-26 22:35:16 +0930

    Update model

--- lib/models/daily_intake.dart
+++ lib/models/daily_intake.dart
@@ -5,15 +5,27 @@ import 'exercise.dart';
 
 // ignore: must_be_immutable
 class DailyIntake extends Equatable {
-  Nutriments? breakfast;
-  Nutriments? lunch;
-  Nutriments? dinner;
-  Nutriments? snack;
-  Exercise? exercise;
+  List<Nutriments>? breakfast;
+  List<Nutriments>? lunch;
+  List<Nutriments>? dinner;
+  List<Nutriments>? snack;
+  List<Exercise>? exercise;
 
-  DailyIntake({this.breakfast, this.lunch, this.dinner, this.snack});
+  DailyIntake({
+    this.breakfast,
+    this.lunch,
+    this.dinner,
+    this.snack,
+    this.exercise,
+  });
 
   getTotalDailyEnergyCal() {}
+  getTotalDailyEnergyKj() {}
+
+  getTotalDailyProtein() {}
+  getTotalDailyCarbohydrates() {}
+  getTotalDailyFats() {}
+  // TODO: Add more gets for other nutrients
 
   @override
   List<Object?> get props => [
@@ -21,5 +33,6 @@ class DailyIntake extends Equatable {
         lunch,
         dinner,
         snack,
+        exercise,
       ];
 }
